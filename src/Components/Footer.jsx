38362e399da0d5df6ba9cad6eaa3f5999be63cbe
Gabriel Castro <gabriel.castro@coreconsulting.com.br>
2020-08-22 21:25:29 -0300

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+function Footer(props) {
 
-function Footer() {
-    return(
-        <div>Footer</div>
-    )
+    if (props.data) {
+        var networks = props.data.social.map(function (network) {
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        })
+    }
+
+    return (
+        <footer>
+
+            <div className="row">
+                <div className="twelve columns">
+                    <ul className="social-links">
+                        {networks}
+                    </ul>
+
+                    <ul className="copyright">
+                        <li>&copy; Copyright 2020 Futur1s</li>
+                        <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Futur1s</a></li>
+                    </ul>
+
+                </div>
+                <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+            </div>
+        </footer>
+    );
 }
+
 export default Footer;
